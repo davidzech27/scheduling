a04d8a51df9c95897ca4aa5716a5a85eca2faf4a
David Zechowy <davidzech27@gmail.com>
2023-12-16 12:48:00 -0800

    fix jose import maybe

--- src/app/SideBar.tsx
+++ src/app/SideBar.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useEffect } from "react"
 
 import defaultSettings from "~/defaultSettings"
 import FocusedBookingSideBarContent from "./FocusedBookingSideBarContent"
@@ -6,10 +7,26 @@ import DefaultSideBarContent from "./DefaultSideBarContent"
 import DraftBookingSideBarContent from "./DraftBookingSideBarContent"
 import FocusedRoomSideBarContent from "./FocusedRoomSideBarContent"
 import FocusedUserSideBarContent from "./FocusedUserSideBarContent"
-import { useFocusedEntity } from "~/client/focus"
+import { useFocusedEntity, useUnfocus } from "~/client/focus"
 
 export default function SideBar() {
 	const focusedEntity = useFocusedEntity()
+
+	const unfocus = useUnfocus()
+
+	useEffect(() => {
+		const handleKeyDown = (e: KeyboardEvent) => {
+			if (e.key === "/" && e.target === document.body) {
+				unfocus()
+			}
+		}
+
+		document.addEventListener("keydown", handleKeyDown)
+
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown)
+		}
+	}, [unfocus])
 
 	return (
 		<div
